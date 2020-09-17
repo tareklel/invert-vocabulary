9be61a1a594c1631602e4dcc59fd7f339e6085cf
Tarek Lel <tarek.lel@farfetch.com>
2020-09-17 12:15:56 +0400

    added terms_series function

--- app/invert.py
+++ app/invert.py
@@ -1,4 +1,4 @@
-# import pandas as pd
+import pandas as pd
 import nltk
 
 
@@ -13,3 +13,13 @@ class Invert:
         listed = [porter.stem(word) for word in splitter]
 
         return listed
+
+    @staticmethod
+    def terms_series(series):
+        """Test transform of series of lists into a series of unique terms"""
+        series_list = list(series)
+        flatlist = [item for sublist in series_list for item in sublist]
+        flatlist = list(set(flatlist))
+        final_series = pd.Series(flatlist).sort_values().reset_index(drop=True)
+
+        return final_series
--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from pandas.util.testing import assert_series_equal
+
 import numpy as np
 
 import unittest
@@ -11,17 +13,27 @@ class InverseTableTests(unittest.TestCase):
     def setUp(self):
         """Sets things up before test. Creates pandas table"""
         self.table = pd.DataFrame(np.array([['I ate a computer',
-                                            'I eat computing books',
-                                            'Computation is fun'],
-                                           [4, 5, 6],
-                                           [4, 8, 9]]),
+                                             'I eat computing books',
+                                             'Computation is fun'],
+                                            [4, 5, 6],
+                                            [4, 8, 9]]),
                                   columns=['a', 'b', 'c'])
+
+        self.series = pd.Series([['dog', 'cat', 'bird'],
+                                ['dog', 'ogre'],
+                                ['ogre', 'dog']])
 
     def test_stems_list(self):
         """Tests if string is transformed into list of stems"""
         string = self.table.loc[0, "a"]
         stems = Invert.stem_list(string)
         self.assertEqual(stems, ['I', 'ate', 'a', 'comput'])
+
+    def test_terms_series(self):
+        """Test transform of series of lists into a series of unique terms"""
+        series = Invert.terms_series(self.series)
+        series_test = pd.Series(['bird', 'cat', 'dog', 'ogre'])
+        assert_series_equal(series, series_test)
 
 
 if __name__ == '__main__':
